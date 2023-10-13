740cd45f07f862270cbbae64ef366c6b31772658
Olivier Bieri <bieriol1@students.zhaw.ch>
2023-10-13 17:33:20 +0200

    NPV Calculation for traditional approach complete

--- Functions.py
+++ Functions.py
@@ -8,43 +8,64 @@ import random
 
 def Scenario_creation(mu, sigma, Dt0, dt=1, Fth=50, Forecasts=20):
     """This function is calculating various scenarios for Demand
+
     Args:
-        mu          Mean Percentage Growth mu
-        sigma       Standart Deviation of Percentage Growth
-        Dt0         Initial Demand t0
-        dt          Duration of Delta t in Years
-        Fth         Forecast time horizon
-        #           number of Forecasts (+1)
+        mu          Mean Percentage Growth mu                       float
+        sigma       Standart Deviation of Percentage Growth         float
+        Dt0         Initial Demand t0                               int
+        dt          Duration of Delta t in Years                    int
+        Fth         Forecast time horizon                           int
+        Forecasts   # number of Forecasts (+1 case we start at 0)   int
     Returns:
-        Szenarios
+        Szenarios                                                   np.array
+
+    To Call the Function use following syntax:
+        Scenario_creation(mu, sigma, Dt0, dt, Fth, Forecasts)
     """
+    # Creation of Number of Forecast Vector
     S = list(range(1, Forecasts + 1))
+    # Creation of a time length of the Scenario Vectors
     S2 = list(range(1, Fth))
 
     # Spread of the Scenario can be adjusted here at randomrange
     randomrange = np.arange(-1, 1, 0.1)
+    # Initialise a Scenarios Vector
     Szenarios = []
-
+    # for loop to iterate over all Forecasts
     for i in S:
+        # Add Demand at t0 to the Vector
         D = [Dt0]
+        # Second for loop to iterate over the time length of the Scenarios
         for j in S2:
             Szenario = D[j - 1] + (
                 D[j - 1] * mu * dt
                 + D[j - 1] * sigma * random.choice(randomrange) * math.sqrt(dt)
             )
+            # Append all individual Demand Curves to the Scenarios
             D.append(Szenario)
+        # Append all Demand Vectors to the Scenarios Maxtrix
         Szenarios.append(D)
-    return Szenarios
+    return np.array(Szenarios)
 
 
 def Scenario_plot(Scenarios, Fth=50):
     """This function is plotting the Scenarios Created in the Scenario function
+
     Args:
-        Szenario Data
-        Fth         Forecast time horizon
+        Scenarios   Szenario Data                  np.array
+        Fth         Forecast time horizon          int
     Returns:
-        Plot of Vectors
+        Plot of all Demand Vectors in a Single Graph
+
+    To Call the Function use following syntax:
+        Scenario_plot(Scenarios, Fth)
     """
+    # If loop when Scenarios is only a Vector
+    if Scenarios.ndim == 1:
+        Scenarios = Scenarios.reshape(1, -1)
+    else:
+        Scenarios = Scenarios
+
     plotvector = list(range(1, (Fth + 1)))
     for scenario in Scenarios:
         plt.plot(plotvector, scenario, label="Scenario")
@@ -62,54 +83,57 @@ def Scenario_plot(Scenarios, Fth=50):
 # t = 1
 # Fth = 50
 # Forecasts = 20
-
-# Scenario_plot(Scenario(mu, sigma, Dt0, dt, Fth, Forecasts), Fth)
+# To Call the Function use this Format:
+# Scenario_plot(Scenario_creation(mu, sigma, Dt0, dt, Fth, Forecasts), Fth)
 
 
 def NPV_Calculation(D, K, t, r_D, r_K, co_K, co_D, ci_K, discount, EoS):
     """This function is calculates the NPV as a function of a Demand and Capacity Vector
-    Args:
-        r_D         Revenues per Unit of Demand per Period
-        r_K         Revenues per Unit of Capacity per Period
-        co_K        Operational costs per unit of capacity per period
-        co_D        Operational cost per unit of demand per period
-        ci_K        Installation cost per unit of capacity
-        discount    Discount factor
-        EoS         Economy of Scale factor
-    Returns:
-        NPV for given Inputs
-    """
-    deltaK = []
-    for i in range(len(K)):
-        if i == 0:
-            # For the first element, just append it as is.
-            deltaK.append(0)
-        else:
-            # For subsequent elements, subtract the previous element.
-            difference = K[i] - K[i - 1]
-            deltaK.append(difference)
 
-    Revenue = []
-    Cost = []
-    Profit = []
-    Discount = []
-    for i in range(len(K)):
-        # Revenue
-        R = r_D * D[i] + r_K * K[i]
-        Revenue.append(R)
-        # Cost
-        # Investment costs
-        CI = ci_K * deltaK[i] ** EoS
-        # Operational costs
-        CO = co_K * K[i] + co_D * D[i]
-        C = CI + CO
-        Cost.append(C)
-        # Profits
-        P = R - C
-        Profit.append(R)
-        # Discount
-        PV = (1 / (1 + discount) ** t) * P
-        Discount.append(PV)
-    NPVs = sum(Discount)
-    NPV = sum(NPVs)
+    Args:
+        D           Demand Vector                                       np.array
+        K           Estimated Capacity Vector                           np.array
+        r_D         Revenues per Unit of Demand per Period              float
+        r_K         Revenues per Unit of Capacity per Period            float
+        co_K        Operational costs per unit of capacity per period   flaot
+        co_D        Operational cost per unit of demand per period      float
+        ci_K        Installation cost per unit of capacity              float
+        discount    Discount factor                                     float
+        EoS         Economy of Scale factor                             float
+
+    Returns:
+        NPV for given Inputs                                            np.array
+
+    To call this Function use following syntax:
+        NPV_Calculation(D, K, t, r_D, r_K, co_K, co_D, ci_K, discount, EoS)
+    """
+    # Creation of a Capacity Change Vector
+    deltaK0 = np.diff(K)
+    # Setting the initial Value of the Change Vector to Zero
+    deltaK = np.insert(deltaK0, 0, 0)
+
+    # If loop when the Demand Matrix is only a Vector
+    if D.ndim == 1:
+        D = D.reshape(1, -1)
+    else:
+        D = D
+    NPV = np.zeros(D.shape[0])  # Initialize an array to store NPV for each row
+    # For loop to iterate over all Scenarios
+    for i in range(D.shape[0]):
+        #  Revenue as function of Demand and Capacity
+        Revenue = r_D * D[i] + r_K * K
+        # Operationals Costs as a function of Demand and Capacity
+        Cost_Ops = co_K * K + co_D * D[i]
+        # Investment Costs as a function of Capacity and Economys of Scale Factor
+        Cost_Investment = ci_K * (np.power(deltaK, EoS))
+        # Total Cost as Sum of Operational and Investment Costs
+        Cost = Cost_Ops + Cost_Investment
+        # Profit as Difference of Revenue and Total Cost
+        Profit = Revenue - Cost
+        # Discount rate as a function of the Discount Factor and time t
+        Discount = 1 / (1 + discount) ** t
+        # Present Value as a function of Discount rate and Profit
+        Present_Value = Discount * Profit
+        # Net Present Value as sum of all Present Values
+        NPV[i] = np.sum(Present_Value)
     return NPV
